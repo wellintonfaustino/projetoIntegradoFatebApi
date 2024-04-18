5e5e313964df11d458fc68c0bda359f04cfc4301
Wellinton - PC Klin <wellinton@null.net>
2024-04-18 09:07:56 -0300

    Retirado Required do atualizar produtos

--- endpoints/get/getAllProduto.js
+++ endpoints/get/getAllProduto.js
@@ -5,16 +5,40 @@ function GetTodosProdutos() {
    app.get('/produtos', (req, res) => {
       const con = mysqlConnection();
 
-      con.query('SELECT * FROM produto', (error, results) => {
-         if (error) {
-            console.error('Erro ao executar consulta:', error);
-            return res.status(500).json({
-               error: 'Ocorreu um erro ao tentar obter as categorias.',
-            });
-         }
+      con.query(
+         `select produto.id 
+         , produto.nome_produto 
+         , produto.preco 
+         , produto.id_categoria 
+         , produto.id_fornecedor 
+         , categoria.nome_categoria 
+         , fornecedor.nome_fornecedor
+         , count(produto_pedido.id) qnt_pedido
+      from produto
+      left join categoria
+        on categoria.id = produto.id_categoria  
+      left join fornecedor
+        on fornecedor.id  = produto.id_fornecedor
+      left join produto_pedido
+        on produto_pedido.id_produto  = produto.id
+     group by produto.id 
+         , produto.nome_produto 
+         , produto.preco 
+         , produto.id_categoria 
+         , produto.id_fornecedor 
+         , categoria.nome_categoria 
+         , fornecedor.nome_fornecedor`,
+         (error, results) => {
+            if (error) {
+               console.error('Erro ao executar consulta:', error);
+               return res.status(500).json({
+                  error: 'Ocorreu um erro ao tentar obter as categorias.',
+               });
+            }
 
-         res.json(results);
-      });
+            res.json(results);
+         },
+      );
       // Fechando a conexão com o MySQL
       con.end();
    });
--- endpoints/put/putProdutos.js
+++ endpoints/put/putProdutos.js
@@ -12,30 +12,53 @@ function PutProduto() {
          });
       }
 
+      // Construir a consulta SQL baseada nos campos não nulos
+      let query = 'UPDATE produto SET';
+      const values = [];
+
+      if (nome_produto) {
+         query += ' nome_produto = ?,';
+         values.push(nome_produto);
+      }
+
+      if (id_categoria) {
+         query += ' id_categoria = ?,';
+         values.push(id_categoria);
+      }
+
+      if (preco) {
+         query += ' preco = ?,';
+         values.push(preco);
+      }
+
+      if (id_fornecedor) {
+         query += ' id_fornecedor = ?,';
+         values.push(id_fornecedor);
+      }
+
+      // Remover a última vírgula da consulta SQL
+      query = query.slice(0, -1);
+
+      // Adicionar a cláusula WHERE para atualizar apenas o fornecedor com o ID fornecido
+      query += ' WHERE id = ?';
+      values.push(id);
+
       const con = mysqlConnection();
 
-      con.query(
-         `UPDATE produto
-         SET nome_produto=?, preco=?, id_categoria=?, id_fornecedor=?
-         WHERE id=?; `,
-         [nome_produto, preco, id_categoria, id_fornecedor, id],
-         (error, results) => {
-            if (error) {
-               console.error('Erro ao executar consulta:', error);
-               return res.status(500).json({
-                  error: 'Ocorreu um erro ao tentar atualizar o Produto.',
-               });
-            }
+      con.query(query, values, (error, results) => {
+         if (error) {
+            console.error('Erro ao executar consulta:', error);
+            return res.status(500).json({
+               error: 'Ocorreu um erro ao tentar atualizar o Produto.',
+            });
+         }
 
-            if (results.affectedRows === 0) {
-               return res
-                  .status(404)
-                  .json({ error: 'Produto não encontrado.' });
-            }
+         if (results.affectedRows === 0) {
+            return res.status(404).json({ error: 'Produto não encontrado.' });
+         }
 
-            res.json({ message: 'Produto atualizado com sucesso.' });
-         },
-      );
+         res.json({ message: 'Produto atualizado com sucesso.' });
+      });
       // Fechando a conexão com o MySQL
       con.end();
    });
